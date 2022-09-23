c9b8e31bda43ac23b0454a059ce1ea83b7b16ffd
moneshvenkul <mvvenkul@gmail.com>
2022-09-23 11:25:58 -0400

    added NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home.js';
-import About from './pages/About.js';
-import Article from './pages/Article.js';
-import ArticlesList from './pages/ArticlesList.js';
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Article from "./pages/Article.js";
+import ArticlesList from "./pages/ArticlesList.js";
 
-import NavBar from './components/NavBar.js';
-
+import NavBar from "./components/NavBar.js";
 function App() {
   return (
-    <div className="max-w-screen-md mx-auto pt-20">
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/articles-list" element={<ArticlesList />} />
-      <Route path="/article" element={<Article />} />
-      </Routes>
+      <NavBar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles-list" element={<ArticlesList />} />
+          <Route path="/article" element={<Article />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-    </div>
   );
 }
 
