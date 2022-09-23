b5d51f6c7dbd3edc034eb901bb061a502e9512e0
moneshvenkul <mvvenkul@gmail.com>
2022-09-23 11:52:06 -0400

    added url param to article

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Article = () => {
+    const name = useParams();
   return (
     <div className="mb-20">
       <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">
-      Article
+      This is {name.name} Article
       </h1>
     </div>
   );
