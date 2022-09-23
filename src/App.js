6fef43fda1f4b36a989670183964cb17af7f072b
moneshvenkul <mvvenkul@gmail.com>
2022-09-23 10:51:49 -0400

    created pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import Home from './pages/Home.js'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+import Article from './pages/Article.js';
+import ArticlesList from './pages/ArticlesList.js';
 
 
 function App() {
@@ -8,6 +12,9 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/articles-list" element={<ArticlesList />} />
+      <Route path="/article" element={<Article />} />
       </Routes>
     </BrowserRouter>
     </div>
