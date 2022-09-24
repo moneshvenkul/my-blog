1a034113f8e7db29d378ae2563ef98032f5876ae
moneshvenkul <mvvenkul@gmail.com>
2022-09-24 10:52:53 -0400

    added Articles List, fixed images

--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -2,16 +2,19 @@ const articles = [
   {
     name: "learn-react",
     title: "The Fastest way to learn React",
+    thumbnail:'/images/blog1',
     content: ["Hello Welcome to this article for learning react quick."],
   },
   {
     name: "monesh",
     title: "The Fastest way to learn web",
+    thumbnail:'/images/blog2',
     content: ["Hello Welcome to this article for learning web quick."],
   },
   {
     name: "learn-js",
     title: "The Fastest way to learn js",
+    thumbnail:'/images/blog3',
     content: ["Hello Welcome to this article for learning js quick."],
   },
 ];
