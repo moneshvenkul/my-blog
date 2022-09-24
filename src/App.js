b6a42e680de0100ba7b364cdd4c4b68139fb4853
moneshvenkul <mvvenkul@gmail.com>
2022-09-24 11:45:49 -0400

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 import Article from "./pages/Article.js";
 import ArticlesList from "./pages/ArticlesList.js";
+import NotFound from "./pages/NotFound.js";
 
 import NavBar from "./components/NavBar.js";
+
 function App() {
   return (
     <BrowserRouter>
@@ -16,7 +19,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/articles-list" element={<ArticlesList />} />
           <Route path="/article/:name" element={<Article />} />
+          <Route path="*" element={<NotFound/>} />
         </Routes>
+       
       </div>
     </BrowserRouter>
   );
